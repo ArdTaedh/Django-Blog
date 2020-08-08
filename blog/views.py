c9c877f15c3f35136139c3e85099f3a7b750d9ea
miltiades <bvzyuk@gmail.com>
2020-08-08 16:22:47 +0300

    Abilitty to delete posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 
 #def about(request):
@@ -30,3 +31,9 @@ class UpdatePostView(UpdateView):
     model = Post
     template_name ='blog/update_post.html'
     fields = ['title', 'body']
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'blog/delete_post.html'
+    success_url = reverse_lazy('index')
